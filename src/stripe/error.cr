596fedd26ea7f4254cdd6d265411dd5d85839e3d
Vlad Faust <mail@vladfaust.com>
2018-11-06 22:53:04 +0300

    feat: better Stripe::Error serialization

--- src/stripe/error.cr
+++ src/stripe/error.cr
@@ -1,9 +1,25 @@
 class Stripe::Error < Exception
+  enum Type
+    ApiConnectionError
+    ApiError
+    AuthenticationError
+    CardError
+    IdempotencyError
+    InvalidRequestError
+    RateLimitError
+  end
+
   JSON.mapping({
-    code:    String?,
-    doc_url: String?,
-    message: String?,
-    param:   String?,
-    type:    String,
+    type: {
+      type:      Type,
+      converter: Enum::StringConverter(Type),
+    },
+    charge:       String?,
+    code:         String?,
+    decline_code: String?,
+    doc_url:      String?,
+    message:      String?,
+    param:        String?,
+    # source:       Source?, # TODO
   })
 end
