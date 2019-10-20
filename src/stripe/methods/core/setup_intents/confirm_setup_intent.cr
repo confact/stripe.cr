6b1c1b0ae9d088232e5253f1b0958d732ecd5655
Håkan Nylén <hakan@dun.se>
2019-10-20 19:24:13 +0200

    added possibility to confirm setupintent

---
+++ src/stripe/methods/core/setup_intents/confirm_setup_intent.cr
@@ -0,0 +1,29 @@
+class Stripe
+  def confirm_setup_intent(
+    intent : String | SetupIntent? = nil,
+    payment_method : String | Token | PaymentMethods::Card | PaymentMethods::BankAccount? = nil,
+    return_url : String? = nil,
+  ) : SetupIntent forall T, U
+    intent = intent.as(SetupIntent).id if intent.is_a?(SetupIntent)
+
+    case payment_method
+    when Token, PaymentMethods::Card, PaymentMethods::BankAccount
+      payment_method = payment_method.not_nil!.id
+    end
+
+    io = IO::Memory.new
+    builder = ParamsBuilder.new(io)
+
+    {% for x in %w(payment_method return_url) %}
+      builder.add({{x}}, {{x.id}}) unless {{x.id}}.nil?
+    {% end %}
+
+    response = @client.post("/v1/setup_intents/#{intent}/confirm", form: io.to_s)
+
+    if response.status_code == 200
+      return SetupIntent.from_json(response.body)
+    else
+      raise Error.from_json(response.body, "error")
+    end
+  end
+end
