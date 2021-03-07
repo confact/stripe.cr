04e7994d588d6681f9a32f9bf7980e7ea322a050
Xavi Ablaza <xlablaza@gmail.com>
2021-03-06 21:35:37 -0800

    Try mapping the keys correctly

--- src/stripe/objects/core/event.cr
+++ src/stripe/objects/core/event.cr
@@ -7,13 +7,16 @@ class Stripe::Event
 
   @[JSON::Field(converter: Time::EpochConverter)]
   getter created : Time?
+
+  @[JSON::Field(key: "object")]
+  getter object_type : String?
+
+  @[JSON::Field(root: "data", key: "object")]
+  getter object : Stripe::Checkout::Session? | Stripe::Subscription?
+
+  getter api_version : String?
   getter id : String?
   getter type : String?
-  getter object : String? | Stripe::Checkout::Session? | Stripe::Subscription?
-  getter amount : Int32?
-  getter client_secret : String?
-  getter currency : String?
-  getter flow : String?
   getter livemode : Bool?
-  getter metadata : Hash(String, String)?
+  getter pending_webhooks : Int32?
 end
