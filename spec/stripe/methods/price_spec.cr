1660b4d53a35912e2ee607f4e6eeceb88ba2be07
Rémy Marronnier <rmarronnier@gmail.com>
2021-05-15 21:05:55 +0200

    Done

---
+++ spec/stripe/methods/price_spec.cr
@@ -0,0 +1,11 @@
+require "../../spec_helper"
+
+describe Stripe::Price do
+  it "listing prices" do
+    WebMock.stub(:get, "https://api.stripe.com/v1/prices")
+      .to_return(status: 200, body: File.read("spec/support/list_prices.json"), headers: {"Content-Type" => "application/json"})
+
+    prices = Stripe::Price.list
+    prices.first.id.should eq("price_1IqjDxJN5FrkuvKhKExUK1B2")
+  end
+end
