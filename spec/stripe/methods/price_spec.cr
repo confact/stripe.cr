347ed1ab41e8feb55a580090ede32a7ad301e71f
Håkan Nylén <confact@users.noreply.github.com>
2023-01-08 15:41:58 +0100

    fix list method for params (#58)

--- spec/stripe/methods/price_spec.cr
+++ spec/stripe/methods/price_spec.cr
@@ -8,4 +8,13 @@ describe Stripe::Price do
     prices = Stripe::Price.list
     prices.first.id.should eq("price_1IqjDxJN5FrkuvKhKExUK1B2")
   end
+
+  it "listing prices with params" do
+    WebMock.stub(:get, "https://api.stripe.com/v1/prices")
+      .with(body: "currency=AUD")
+      .to_return(status: 200, body: File.read("spec/support/list_prices.json"), headers: {"Content-Type" => "application/json"})
+
+    prices = Stripe::Price.list(currency: "AUD")
+    prices.first.id.should eq("price_1IqjDxJN5FrkuvKhKExUK1B2")
+  end
 end
